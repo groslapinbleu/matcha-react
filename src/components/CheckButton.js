cc41bfd488a7c43518dbba8855906d8239c307c5
groslapinbleu <antoine.jouannais@gmail.com>
2020-12-10 19:32:45 +0100

    changes to Chat page: it is now possible to delete a message. Added the Message component

--- src/components/CheckButton.js
+++ src/components/CheckButton.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Check from '../Icons/Check'
 
 const CheckButton = ({onClick}) => {
+    // if onClick prop is not provided, the button is used in a form and needs to be of type submit
     return (
-        <button type="button" onClick={onClick}><Check /></button>
+        onClick
+        ? <button type="button" onClick={onClick}><Check /></button>
+        : <button type="submit" ><Check /></button>
     )
 }
 
--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Message from "../components/Message"
 import Spinner from 'react-loader-spinner'
+import CheckButton from '../components/CheckButton';
+import { isEmptyString } from '../helpers/validation'
 import { withFirebase } from '../services/Firebase'
 
 class Chat extends Component {
@@ -12,7 +15,8 @@ class Chat extends Component {
       messages: [],
       text: '',
       error: null,
-      loadingMessages: false
+      loadingMessages: false,
+      limit: 10
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,24 +24,39 @@ class Chat extends Component {
   }
 
   async componentDidMount() {
+    this.onListenForMessages()
+  }
+
+  onListenForMessages = () => {
     const { auth, messages } = this.props.firebase
     this.setState({ user: auth.currentUser })
     this.setState({ error: null, loadingMessages: true });
     const chatArea = this.myRef.current;
     try {
-        messages().on("value", snapshot => {
-        let messages = [];
-        snapshot.forEach((snap) => {
-          messages.push(snap.val());
-        });
-        messages.sort(function (a, b) { return a.createdAt - b.createdAt; });
-        this.setState({ messages });
-        chatArea.scrollBy(0, chatArea.scrollHeight);
-        this.setState({ loadingMessages: false });
-      });
+      messages()
+        .orderByChild('createdAt')
+        .limitToLast(this.state.limit)
+        .on("value", snapshot => {
+          const messageObject = snapshot.val();
+
+          if (messageObject) {
+            const messages = Object.keys(messageObject).map(key => ({
+              ...messageObject[key],
+              uid: key,
+            }))
+            this.setState({ messages, loadingMessages: false });
+            chatArea.scrollBy(0, chatArea.scrollHeight);
+          } else {
+            this.setState({ messages: [], loadingMessages: false })
+          }
+        })
     } catch (error) {
       this.setState({ error: error.message, loadingMessages: false });
     }
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.messages().off();
   }
 
   handleChange(event) {
@@ -48,53 +67,60 @@ class Chat extends Component {
 
   async handleSubmit(event) {
     event.preventDefault();
-    const { messages } = this.props.firebase
-    this.setState({ error: null });
-    const chatArea = this.myRef.current;
-    try {
-      await messages().push({
-        // await setArrayValue("messages",{
-        // db.ref(ref).push(data)
-        text: this.state.text,
-        createdAt: Date.now(),
-        userId: this.state.user.uid,
-        displayName: this.state.user.displayName
-      });
-      this.setState({ text: '' });
-      chatArea.scrollBy(0, chatArea.scrollHeight);
-    } catch (error) {
-      this.setState({ error: error.message });
+    if (!isEmptyString(this.state.text)) {
+      const { messages } = this.props.firebase
+      this.setState({ error: null });
+      const chatArea = this.myRef.current;
+      try {
+        await messages().push({
+          // await setArrayValue("messages",{
+          // db.ref(ref).push(data)
+          text: this.state.text,
+          createdAt: Date.now(),
+          userId: this.state.user.uid,
+          displayName: this.state.user.displayName
+        });
+        this.setState({ text: '' });
+        chatArea.scrollBy(0, chatArea.scrollHeight);
+      } catch (error) {
+        this.setState({ error: error.message });
+      }
     }
   }
 
-  formatTime(timestamp) {
-    const d = new Date(timestamp);
-    const time = `${d.getDate()}/${(d.getMonth() + 1)}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`;
-    return time;
+
+  onNextPage = () => {
+    this.setState(
+      state => ({ limit: state.limit + 5 }),
+      this.onListenForMessages,
+    )
+  }
+
+  onRemoveMessage = uid => {
+    console.log("trying to remove message with uid = " + uid)
+    this.props.firebase.message(uid).remove()
   }
 
   render() {
     return (
       <div className="">
+
         <Header />
-        <div className="mt-16 p-6 h-2/3 overflow-y-scroll bg-indigo-50" ref={this.myRef}>
+        <button type="button" className="m-2 p-2 rounded-md bg-indigo-200 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white" onClick={this.onNextPage}>Get more messages</button>
+
+        <div className="mt-8 p-6 h-2/3 overflow-y-scroll bg-indigo-50" ref={this.myRef}>
           {this.state.loadingMessages
             ? <div className="flex items-center justify-center"><Spinner type='Puff' color='#038E9F' height={50} width={50} /></div>
             : ""}
           {/* chat area */}
           {this.state.messages.map(chat => {
-            return <div key={chat.createdAt} className={"p-1 m-2 max-w-2xl rounded-lg break-words speech-bubble " + (this.state.user.uid === chat.userId
-              ? "bg-green-300 text-white ml-auto"
-              : "bg-gray-200")}>
-              {chat.text}
-              <span className="text-xs float-right">{(this.state.user.uid === chat.userId ? "myself" : chat.displayName)} - {this.formatTime(chat.createdAt)}</span>
-              <div className=""></div>
-            </div>
+            return <Message key={chat.createdAt} user={this.state.user} chat={chat} onRemoveMessage={this.onRemoveMessage}></Message>
           })}
         </div>
-        <form onSubmit={this.handleSubmit} className="mx-auto sticky bottom-0 max-w-2xl">
-          <textarea className="border-solid border-2 w-full rounded-lg " name="text" onChange={this.handleChange} value={this.state.text}></textarea>
-          <button type="submit" className="p-2 rounded-md bg-indigo-200 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white">Send</button>
+        <form onSubmit={this.handleSubmit} className="flex mx-auto max-w-2xl">
+          <textarea className="border-solid border-2 w-full rounded-lg " name="text" onChange={this.handleChange} value={this.state.text} ></textarea>
+          {/* <button type="submit" className="p-2 rounded-md bg-indigo-200 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white">Send</button> */}
+          <CheckButton />
           {this.state.error ? <p className="text-red-500">{this.state.error}</p> : null}
         </form>
         <Footer></Footer>
