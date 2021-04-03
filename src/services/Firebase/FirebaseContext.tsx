73fc84eab6bbe83f5f4f3f78ec1263adb9b9c02e
groslapinbleu <antoine.jouannais@gmail.com>
2021-04-03 12:20:20 +0200

    added TypeScript support; I can bow mix TS and JS

--- src/services/Firebase/FirebaseContext.tsx
+++ src/services/Firebase/FirebaseContext.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const FirebaseContext = React.createContext(null);
+const FirebaseContext = React.createContext<Object | null>(null);
 
 // eslint-disable-next-line react/display-name
-export const withFirebase = (Component) => (props) => (
+export const withFirebase = (Component: any) => (props: any) => (
   <FirebaseContext.Consumer>
     {(firebase) => <Component {...props} firebase={firebase} />}
   </FirebaseContext.Consumer>
